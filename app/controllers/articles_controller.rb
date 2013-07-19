088c6848b186b84b5add8cb7f78453a3fe102a45
Eric Mooney <eric.c.mooney@gmail.com>
2013-07-19 11:32:23 -0700

    Add infinite scroll

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @articles }
+      format.js
     end
   end
 
