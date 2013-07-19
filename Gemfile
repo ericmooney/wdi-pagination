d14071e6f461963c96d470a21fd652cdedc31405
Eric Mooney <eric.c.mooney@gmail.com>
2013-07-19 10:12:32 -0700

    Add gem will-paginate, and removed earlier logic

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem 'binding_of_caller'
 gem 'meta_request'
 gem 'annotate'
 
-
+gem 'will_paginate'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,15 +2,9 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles_count = Article.count
-
-    # page 2 -1 = 1 x 5 = 5
     @per_page = 5
-    #to get the page you are on, you can set up a route to show you that.. http://localhost:5000/articles?page=2, etc
-    @current_page = params[:page || 1].to_i
-    @page_offset = (@current_page - 1) * @per_page
 
-    @articles = Article.limit(5).offset(5)
+    @articles = Article.page(params[:page]).per_page(@per_page)
 
     respond_to do |format|
       format.html # index.html.erb
