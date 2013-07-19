b244b9b4dc354435d1b2bdc5849047ed907cb509
Sumeet Jain <sumeet@sumeetjain.com>
2013-07-19 00:02:37 -0700

    Endless Scroll

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,2 +1,29 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function(){
+  // While there is still pagination on the page:
+  if ($('.pagination').length) {
+
+    // Detect the 'scroll' event:
+    $(window).scroll(function() {
+
+      // Get the next URL from pagination.
+      var url = $('.pagination .next_page').attr('href');
+
+      // $(window).scrollTop() represents where I've scrolled to.
+      // $(document).height() represents the height of the document content, which changes.
+      // $(window).height() - represents the screen height, which does not change.
+      //
+      // The final calculation is then whenever I scroll past 50px form the bottom.
+      //
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+        $('.pagination').text("Fetching more articles...");
+
+        // http://api.jquery.com/jQuery.getScript/
+        // Alias for $.ajax, assumes dataType:"script".
+        return $.getScript(url);
+      }
+    });
+
+    // Trigger a scroll off the bat.
+    return $(window).scroll();
+  }
+});
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @articles }
+      format.js
     end
   end
 
